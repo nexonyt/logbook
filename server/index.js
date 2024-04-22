2f3e65d33a84047c389701d130c9727aecb072c9
nexonyt <kaczmarekm090@gmail.com>
2024-04-22 12:59:31 +0200

    changes

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
-const cors = require('cors');
+const express = require("express");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+const { mongoose } = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Database Connected!"))
+  .catch((err) => console.log("Not connected", err));
 
 const app = express();
 const port = 8000;
-app.listen(port, () => console.log(`Server is running on port ${port}`))
+app.listen(port, () => console.log(`Server is running on port ${port}`));
 
-app.use('/',require('./routes/authRoutes'))
+app.use("/", require("./routes/authRoutes"));
