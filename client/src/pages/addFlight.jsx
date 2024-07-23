93a26cf7662c9fcc766af26455701b2aaa317e25
Marcin Kaczmarek <marcin.kaczmarek@przelewy24.pl>
2024-07-23 10:54:47 +0200

    changed profile

--- client/src/pages/addFlight.jsx
+++ client/src/pages/addFlight.jsx
@@ -274,6 +274,7 @@ export default function AddFlight() {
                     })
                   }
                 />
+                {/* kom */}
               </div>
               <div>
                 <label
